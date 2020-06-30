f11bf1702e25e6e6b8adf76f633143c3484f25d0
Chidi Williams <williamschidi1@gmail.com>
2020-06-30 09:54:58 +0100

    Add start.lisp

---
+++ start.lisp
@@ -0,0 +1,15 @@
+;;; Computes the value of the nth
+;;; number in the Fibonacci sequence
+(defun fibonacci (n)
+  (cond
+    ((= n 0) 0)
+    ((= n 1) 1)
+    (T (do
+        ((curr 1)
+         (ppn 0)
+         (pn 1)
+         (i 1 (1+ i)))
+        ((= i n) curr)
+         (setq curr (+ ppn pn)
+               ppn pn
+               pn curr)))))
