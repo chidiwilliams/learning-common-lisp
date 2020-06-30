ca005d199a2ace9b551cdb85317d56f7c69a299e
Chidi Williams <williamschidi1@gmail.com>
2020-06-30 10:17:16 +0100

    Updated fibonacci.lisp

--- fibonacci.lisp
+++ fibonacci.lisp
@@ -5,10 +5,7 @@
     ((= n 0) 0)
     ((= n 1) 1)
     (T (do
-        ((curr 1)
-         (ppn 0)
-         (pn 1)
-         (i 1 (1+ i)))
+        ((curr 1) (ppn 0) (pn 1) (i 1 (1+ i)))
         ((= i n) curr)
          (setq curr (+ ppn pn)
                ppn pn
